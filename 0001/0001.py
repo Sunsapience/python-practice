057a17eb8529022bef1477d81058ee6f767ef668
Wang Longgang <wlgzglb@outlook.com>
2017-11-28 20:20:32 +0800

    Add files via upload

---
+++ 0001/0001.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 20:12:49 2017
+
+@author: wlgzg
+"""
+
+import random
+import string
+
+forSelect = string.ascii_letters + string.digits
+
+
+def generate_code(count, length):
+    for x in range(count):
+        Re = ""
+        for y in range(length):
+            Re += random.choice(forSelect)
+        print(Re)
+
+if __name__ == '__main__':
+
+    generate_code(20, 5)
