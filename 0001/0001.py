70c44c0bb6631f5687ffa4061145bb796b17a6d9
Wang Longgang <wlgzglb@outlook.com>
2017-11-28 20:44:05 +0800

    Add files via upload

---
+++ 0001/0001.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 28 20:12:49 2017
+
+@author: wlgzg
+"""
+
+import random
+import string
+
+forSelect = string.ascii_letters + string.digits
+
+def generate_code(count, length):
+    f = open('Activation_code.txt', 'w')
+    for x in range(count):
+        Re = ""
+        for y in range(length):
+            Re += random.choice(forSelect)
+        f.write(Re+'\n')
+        print(Re) 
+    f.close()
+
+if __name__ == '__main__':
+    generate_code(200, 20)
