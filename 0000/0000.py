6176237655294e9bc6621183148d86904278c8f5
Wang Longgang <wlgzglb@outlook.com>
2017-11-28 11:51:05 +0800

    Update 0000.py

--- 0000/0000.py
+++ 0000/0000.py
@@ -1,1 +1,22 @@
+from PIL import Image, ImageDraw, ImageFont
 
+
+def img_addnum(img_name, num):
+    im = Image.open(img_name)
+    draw = ImageDraw.Draw(im)
+
+    #width and height
+    w = im.width;
+    h = im.height;
+    print( h, w)
+    
+    #load font
+    #fnt = ImageFont.load_default()
+    fnt = ImageFont.truetype('arial.ttf', int(h * 0.15))
+
+    
+    draw.text((w * 0.91 , h * 0.01), num, font=fnt, fill=(255, 0, 0, 128))
+    im.save(img_name.split('.')[0] + '4.jpg')
+
+if __name__ == '__main__':
+    img_addnum('Corong.jpg', '3')
